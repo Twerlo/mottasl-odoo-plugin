6ffac918d5f7c600352274b49f31df72b752fb91
Yusef Nsar <117655727+YusufNasar@users.noreply.github.com>
2023-05-02 00:51:57 +0300

    fix: typo in cover image path

--- mottasl_crm/__manifest__.py
+++ mottasl_crm/__manifest__.py
@@ -7,7 +7,7 @@
     'category': 'Sales/CRM',
     'summary': 'Use Mottasl WA messaging in your CRM application',
     'depends': ['crm'],
-    'images': ['images/main_screenshot.jpg'],
+    'images': ['images/main_screenshot.png'],
     'data': [
         'views/crm_lead_views.xml',
         'views/crm_case_tree_view_oppor.xml',
